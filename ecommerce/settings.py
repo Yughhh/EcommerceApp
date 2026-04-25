965b00b7d6e55b992dbca924044a3c995c9a2d0d
Yughhh <yugh.rana.11@gmail.com>
2026-04-26 00:48:01 +0530

    Fix static files logo issue

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -3,6 +3,13 @@ from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'store/static'),
+]
 SECRET_KEY = 'django-insecure-change-me-in-production-xyz123'
 
 DEBUG = True
@@ -24,6 +31,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
